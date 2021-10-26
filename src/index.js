9c458c33c039625a3db3b2b917cf06addaede97a
Dico Diaz <dicodiaz@gmail.com>
2021-10-26 12:10:55 -0500

    Add deployment without HashRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './functionBased/App.css';
 import TodoContainer from './functionBased/components/TodoContainer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
